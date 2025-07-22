4f50ae79567ad7b032ac32a8b9289ab72e3e7cca
Vladislav Dybok <eden91hazard@gmail.com>
2025-07-23 00:37:55 +0300

    Добавил интеграцию API-сервиса с Keycloak

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,23 +1,23 @@
 import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
-// import session from 'express-session';
-// import Keycloak, { Token } from 'keycloak-connect';
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 import { auth } from './middlewares/auth';
 import { checkIfProtheticUser } from './middlewares/checkIfProtheticUser';
 import errorHandler from './middlewares/errorHandler';
 
-// const memoryStore = new session.MemoryStore();
-// const kcConfig = {
-//   realm: 'reports-realm',
-//   'bearer-only': true,
-//   'auth-server-url': 'http://localhost:8080/',
-//   'ssl-required': 'external',
-//   'resource': 'reports-api',
-//   'confidential-port': 0
-// };
+const memoryStore = new session.MemoryStore();
+const keycloakConfig = {
+  realm: 'reports-realm',
+  'bearer-only': true,
+  'auth-server-url': 'http://localhost:8080/',
+  'ssl-required': 'external',
+  'resource': 'reports-api',
+  'confidential-port': 0
+};
 
-// const keycloak = new Keycloak({ store: memoryStore }, kcConfig);
+const keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
 
 const app = express();
 const PORT = Number(process.env.PORT) || 8000;
@@ -25,10 +25,12 @@ const FRONTEND_HOST = process.env.FRONTEND_HOST;
 
 app.use(cors({origin: FRONTEND_HOST}))
 
+app.use(keycloak.middleware())
+
 // Все роуты, указанные ниже этой мидлвари, защищаются проверкой наличия токена авторизации
 app.use(auth);
 
-app.get('/reports', checkIfProtheticUser, (_, res) => {
+app.get('/reports', keycloak.protect(), checkIfProtheticUser, (_, res) => {
   // TODO: возвращать моковые данные
   res.send(200);
 });
